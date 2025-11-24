330bd40c04bc632948af5f4e88e1eba7b92ae0ad
Abiathar Corsino <abiatharcorsino@gmail.com>
2025-11-24 12:01:51 +0800

    kjdks

--- src/app/URDS/Mamas/URDS-LP/page.tsx
+++ src/app/URDS/Mamas/URDS-LP/page.tsx
@@ -191,7 +191,7 @@ const handleSpecificYearSubmit = () => {
                 <Search className="w-5 h-5 text-gray-400 absolute left-4 top-1/2 -translate-y-1/2" />
                 <input
                   type="text"
-                  placeholder="Search proposals..."
+                  placeholder="Search Announcements..."
                   value={searchQuery}
                   onChange={(e) => setSearchQuery(e.target.value)}
                   className="w-full pl-12 pr-4 py-3 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
